fe67ec7ab3345c0a001c84ccd56434b0c8fd2bcb
dly <dongliya2011@sina.com>
2022-09-13 17:45:22 +0800

    1、分析了flatten_layer的实现

--- src/caffe/layers/flatten_layer.cpp
+++ src/caffe/layers/flatten_layer.cpp
@@ -9,19 +9,25 @@ void FlattenLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   CHECK_NE(top[0], bottom[0]) << this->type() << " Layer does not "
       "allow in-place computation.";
+  //从哪个轴开始平铺（该轴前面的都保留原状）
   const int start_axis = bottom[0]->CanonicalAxisIndex(
       this->layer_param_.flatten_param().axis());
+  //哪个轴结束平铺（该轴后面的都保留原状）
   const int end_axis = bottom[0]->CanonicalAxisIndex(
       this->layer_param_.flatten_param().end_axis());
   vector<int> top_shape;
+  //开始之前的维度不变
   for (int i = 0; i < start_axis; ++i) {
     top_shape.push_back(bottom[0]->shape(i));
   }
+  //计算start_axis 到 end_axis范围的元素数
   const int flattened_dim = bottom[0]->count(start_axis, end_axis + 1);
   top_shape.push_back(flattened_dim);
+  //结束之后的维度不变
   for (int i = end_axis + 1; i < bottom[0]->num_axes(); ++i) {
     top_shape.push_back(bottom[0]->shape(i));
   }
+  //设置新的维度
   top[0]->Reshape(top_shape);
   CHECK_EQ(top[0]->count(), bottom[0]->count());
 }
@@ -29,12 +35,14 @@ void FlattenLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void FlattenLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+  //共享数据		  
   top[0]->ShareData(*bottom[0]);
 }
 
 template <typename Dtype>
 void FlattenLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  //共享梯度	
   bottom[0]->ShareDiff(*top[0]);
 }
 
